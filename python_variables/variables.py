d6625e891da6c1085e5acbeb339bf3c8b0e21d7e
kadelcode <kadelcode@gmail.com>
2022-09-03 17:52:35 +0100

    Python Variables

---
+++ python_variables/variables.py
@@ -0,0 +1,33 @@
+# Variables are memory locations used to store data values.
+
+_name = 'Willow Winks' # variable _name is given a string value: 'Willow Winks'
+
+number = 23 # assigning 23 to a number variable
+
+grade_point = 'A' # grade_point also a variable
+
+
+print(_name)
+
+print(number)
+
+print(grade_point)
+
+
+# Python also allows multiple assignment of variables
+
+# Multiple variables to a single value
+u = v = w = 4
+
+#print respective variables
+print(u)
+print(v)
+print(w)
+
+# Multiple variables to multiple values
+x, y, z = 10, 20, 30
+
+#print respective variables
+print(x)
+print(y)
+print(z)
