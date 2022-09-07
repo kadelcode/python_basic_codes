1ff1a3507f55454186a9651e2c4eab171e817a9a
kadelcode <kadelcode@gmail.com>
2022-09-07 18:37:25 +0100

    Python Data Types

---
+++ python_datatypes/data_types.py
@@ -0,0 +1,32 @@
+# Numbers
+x = 5 # int
+y = 2.2 # float
+z = 2 + 4j # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# String
+name = "John Doe"
+f_name = "Willock"
+#print(name)
+#print(f_name)
+
+
+# List
+list1 = [1,2,3,4]
+list2 = ['Numbers', 2, ['a','b','c']] # A list can contain data with different types
+
+
+# Tuple
+tuple1 = ("Welcome", "Home")
+tuple2 = ("4", "Stacks") # Also contains data with different types
+print(tuple2)
+
+
+# Dictionary(Ordered set of a key-value pair)
+dic = {"first_name":"Kadel", "last_name":"Code"}
+print(dic.keys())
+print(dic.values())
