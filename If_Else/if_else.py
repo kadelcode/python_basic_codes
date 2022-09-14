fadc71b19d8c59f3e2d2e503a65061978f2c2751
kadelcode <kadelcode@gmail.com>
2022-09-14 18:07:02 +0100

    if else statement

---
+++ If_Else/if_else.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number: "))
+
+# The if-block is executed if the condition is true
+if (a % 2 == 0):
+    print("The number is Even")
+
+# The else-block is executed if the if-condition is false
+else:
+    print("The number is Odd")
