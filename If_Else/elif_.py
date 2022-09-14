833b6b4b6cb2e42735d47f5c3c74f2c8885c9108
kadelcode <kadelcode@gmail.com>
2022-09-14 18:14:04 +0100

    Elif Statement

---
+++ If_Else/elif_.py
@@ -0,0 +1,17 @@
+'''
+The elif statement is used to check for multiple conditions
+'''
+
+x = int(input("Enter a number: "))
+
+if (x == 1):
+    print("x = 1")
+
+elif (x == 2):
+    print("x = 2")
+
+elif (x == 3):
+    print("x = 3")
+
+else:
+    print("x is not 1 or 2 or 3")
