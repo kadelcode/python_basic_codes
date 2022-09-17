b63db5003afa4a8b1c0e02e046002894539dc08c
kadelcode <kadelcode@gmail.com>
2022-09-17 18:00:52 +0100

    Break Statement

---
+++ Break_Continue_Pass/_break.py
@@ -0,0 +1,17 @@
+#  BREAK WITH FOR LOOP
+
+list  = [1,2,3,4]
+
+for i in list:
+    if i == 4:
+        break
+    print(i)
+    
+
+# BREAK WITH WHILE LOOP
+i = 0
+while i < 20:
+    if i == 6:
+        break
+    print(i)
+    i += 1
