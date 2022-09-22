e8c1b71ba590a131c051f2f397b700f047031bf2
kadelcode <kadelcode@gmail.com>
2022-09-22 17:42:46 +0100

    Python Lists

---
+++ List/list_.py
@@ -0,0 +1,16 @@
+names = ['John', 'Boel', 'Manic'] # list
+list1 = [1,2,'Big','Small','Maximum']
+
+
+# Accessing Values in Lists
+names[0] # John
+list1[0:3] # [1,2,'Big']
+
+
+# Updating Lists
+names[0] = "James" # names = ['James', 'Boel', 'Manic']
+
+# Delete Element in a list
+del names[0]
+print(names) # names = ['Boel', 'Manic']
+#print(y)
