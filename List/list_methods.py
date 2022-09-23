0a4cf0578be67ebd921281bbc9c709029f9eb92a
kadelcode <kadelcode@gmail.com>
2022-09-23 12:01:22 +0100

    Methods Updated

--- List/list_methods.py
+++ List/list_methods.py
@@ -17,8 +17,18 @@ min(list3)
 
 # append(obj) - appends an object to the list
 list1.append([23,44,'Good'])
-print(list1) # list1 = ['a', 'b', 'c', 'd',[23, 44, 'Good']]
+# list1 = ['a', 'b', 'c', 'd',[23, 44, 'Good']]
 
 # count(obj) - returns count of how many times obj occurs in list
-g = list1.count("a")
-print(g)
+list1.count("a")
+
+# extend(seq) - appends  the contents of seq to the list
+list1.extend([23,44,'Good']) # list1 = ['a', 'b', 'c', 'd', [23, 44, 'Good'], 23, 44, 'Good']
+
+# remove(obj) - removes obj from the list
+list2.remove("Month")
+print(list2) # list2 = ['Six', 1, 5]
+
+# reverse - reverses the objects of the list in place
+list1.reverse()
+print(list1)
